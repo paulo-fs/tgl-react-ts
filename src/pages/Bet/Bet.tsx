f71e58b9151b18984d9ba08815ea2fe2798f45e6
Paulo Souza <jr.paulo87@gmail.com>
2022-07-26 16:56:31 -0300

    structure: created cart elements and styles

--- src/pages/Bet/Bet.tsx
+++ src/pages/Bet/Bet.tsx
@@ -1,6 +1,6 @@
 import { NumbersBtn } from '@components/Buttons/NumbersBtn';
-import { ShoppingCartSimple } from 'phosphor-react';
-import { AddToCart, BetButton, BetContainer, BetNumbers, BetPageContainer, CartContainer, ChooseAGame, FooterButtons, HeaderContainer, SecBtn } from './betStyles';
+import { ArrowRight, ShoppingCartSimple, Trash } from 'phosphor-react';
+import { AddToCart, BetButton, BetContainer, BetNumbers, BetPageContainer, CartContainer, CartContent, CartFooter, CartItem, ChooseAGame, DeleteBtn, FooterButtons, HeaderContainer, SecBtn } from './betStyles';
 
 export function Bet(){
 	function createNumbers(){
@@ -63,7 +63,45 @@ export function Bet(){
 			</BetContainer>
 
 			<CartContainer>
-				<h2>Cart</h2>
+				<CartContent>
+				  <h2>Cart</h2>
+					<div className='cartContent'>
+						<CartItem>
+							<DeleteBtn>
+							  <Trash size={24} />
+							</DeleteBtn>
+							<div className='cartInfos'>
+								<p>
+                  01, 02, 04, 05, 06, 07, 09, 15, 17, 20, 21, 22, 23, 24, 25
+								</p>
+								<h4>Lotofácil <span>R$2,50</span></h4>
+							</div>
+						</CartItem>
+						<CartItem>
+							<DeleteBtn>
+							  <Trash size={24} />
+							</DeleteBtn>
+							<div className='cartInfos'>
+								<p>
+                  01, 02, 04, 05, 06, 07, 09, 15, 17, 20, 21, 22, 23, 24, 25
+								</p>
+								<h4>Lotofácil <span>R$2,50</span></h4>
+							</div>
+						</CartItem>
+					</div>
+				</CartContent>
+
+				<CartFooter>
+					<p>
+            Cart <span>Total: R$7,00</span>
+					</p>
+					<div>
+						<button>
+              Save
+							<ArrowRight color='#27C383'/>
+						</button>
+					</div>
+				</CartFooter>
 			</CartContainer>
 		</BetPageContainer>
 	);
